043335ad22c318fc03d1b0fde011b9ac1010d253
yinhao-f <fengyh07@gmail.com>
2021-12-21 15:52:09 +0800

    Generate key to file

--- src/GenerateKeyToFile.java
+++ src/GenerateKeyToFile.java
@@ -1,9 +1,13 @@
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
 import java.util.Base64;
+import java.util.Date;
 
 public class GenerateKeyToFile {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
@@ -13,19 +17,23 @@ public class GenerateKeyToFile {
         SecretKey secretKey = generator.generateKey();
 
         // Save the key to a file
-        OutputStream f = new FileOutputStream("SecretKey.txt");
+        Date currentDate = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd-HHmmss");
+        String fileName = simpleDateFormat.format(currentDate) + ".key";
+
+        OutputStream f = new FileOutputStream(fileName);
         String keyString = Base64.getEncoder().encodeToString(secretKey.getEncoded());
         f.write(keyString.getBytes(StandardCharsets.UTF_8));
         f.flush();
         f.close();
 
         // Read the secret key
-        InputStream inputStream = new FileInputStream("SecretKey.txt");
-        StringBuilder stringBuilder = new StringBuilder();
-        while (inputStream.available() > 0) {
-            stringBuilder.append((char) inputStream.read());
-        }
-        String keyRead = stringBuilder.toString();
-        System.out.println(keyRead);
+//        InputStream inputStream = new FileInputStream("SecretKey.txt");
+//        StringBuilder stringBuilder = new StringBuilder();
+//        while (inputStream.available() > 0) {
+//            stringBuilder.append((char) inputStream.read());
+//        }
+//        String keyRead = stringBuilder.toString();
+//        System.out.println(keyRead);
     }
 }
